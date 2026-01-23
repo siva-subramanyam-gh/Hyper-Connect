5a125b4590883becf631c0fd4e2d4eb98c2dcc40
siva-subramanyam-gh <ghsivasubramanyam@gmail.com>
2026-01-23 12:44:10 +0530

    Added a newer version with improved Logic

---
+++ V2 (Upgraded)/hyper-server.py
@@ -0,0 +1,54 @@
+import socket 
+import threading
+import time
+import pyperclip
+import ssl
+
+#Defining ports for Broadcast and Data transfer
+Broadcast_port=5001
+Data_port=6000
+Secret_key="HyperConnect_secure_token"  # Simple token for security handshake
+
+def broadcast_presence():
+    '''Sending a UDP beacon for our phone / other devices to identify our presence '''
+    udp=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    udp.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
+    print(f"---Broadcast Beacon Active via Port : {Broadcast_port}")
+    while true:
+        message=f"Hyper Discover:{Data_port}"
+        try:
+            udp.sendto(message.encode(),('<broadcast>',Broadcast_port))
+            time.sleep(2)
+        except Exception as e:
+            print(f"Beacon error:{e}")
+            time.sleep(5)
+def handle_client(conn):
+    """Handles the secure data transfer"""
+    try:
+        data=conn.recv(4096).decode('utf-8')
+        if data.startswith(Secret_key):
+            payload=data.split("::")[1]
+            print(f"Synced {payload}")
+            pyperclip.copy(payload)
+        else:
+            print("Security error:Invalid Token Recieved")
+    except Exception as e:
+        print(f"Transfer error :{e}")
+    finally:
+        conn.close()
+
+def start_data_listener():
+    tcp=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    tcp.bind(('0.0.0.0',Data_port))
+    print(f"Data Listener Active on Port {Data_port}")
+    
+    while True:
+        conn, addr = tcp.accept()
+        threading.Thread(target=handle_client, args=(conn,)).start()
+
+if __name__=="__main__":
+    print("-----Hyper Connect Core-----")
+    t1 = threading.Thread(target=broadcast_presence, daemon=True)
+    t1.start()
+    start_data_listener()
+
